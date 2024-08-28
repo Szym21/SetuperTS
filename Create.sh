293b1e2e3c20c52bff3569f7cb70f1b42eaffb3f
Szymon Aleksiej <szym.aleksiej@gmail.com>
2024-08-28 23:29:59 +0200

    added json edit to reflect build command

--- Create.sh
+++ Create.sh
@@ -3,12 +3,13 @@
 echo 'Hi! Lets create new CLI Typescript project'
 echo '------------------------------------------'
 
+#directory
 read -p "Enter directory name: " dirname
 
 if [ ! -d "$dirname" ]
 then
     echo "Directory doesn't exist. Creating now"
-    mkdir ./$dirname
+    mkdir ../$dirname
     echo "(1/x) Directory created"
 else
     echo "(1/x) Directory exists"
@@ -17,12 +18,15 @@ fi
 echo '(2/x) Changing directory...'
 cd $dirname
 
+#initialize ts
 echo '(3/x) Initializing npm...'
 npm init -y
 
+#add ts
 echo '(4/x) Installing TypeScript...'
 npm install @types/node typescript --save-dev
 
+#add tsconfig
 echo '(5/x) Creating tsconfig...'
 echo '{
   "compilerOptions": {
@@ -37,3 +41,8 @@ echo '{
     "forceConsistentCasingInFileNames": true
   }
 }' >> tsconfig.json
+
+#edit build script in package.json
+echo '(6/x) adding build script...'
+jq '.scripts += {build: "npx tsc"}' package.json > package.json.tmp
+mv package.json.tmp package.json
